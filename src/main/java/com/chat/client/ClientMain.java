cb9c120d6ed07d05ed2a83b9d84b15742bf15596
volnovod <petrus.victor@ukr.net>
2015-11-17 01:10:39 +0200

    add client login-gui and add client authentication

--- src/main/java/com/chat/client/ClientMain.java
+++ src/main/java/com/chat/client/ClientMain.java
@@ -14,28 +14,6 @@ public class ClientMain extends Application {
 
     public static void main(String[] args) {
         launch(args);
-//        try {
-//            Socket socket = new Socket("192.168.1.143", 62000);
-//            try {
-//                InputStream inputStream = socket.getInputStream();
-//                OutputStream outputStream = socket.getOutputStream();
-//                Scanner in = new Scanner(inputStream);
-//                PrintWriter writer = new PrintWriter(outputStream, true);
-//
-//                while (true){
-//                    String line = in.nextLine();
-//                    System.out.println("Server said: " + line);
-//                    System.out.println("enter msg:");
-//                    Scanner scanner = new Scanner(System.in);
-//                    String msg = scanner.nextLine();
-//                    writer.println(msg);
-//                }
-//            } finally {
-//                socket.close();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 
     @Override
